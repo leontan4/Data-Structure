fced66dfe8f2d9a95d6e1a29e98c19d9e1140bcb
Leon Tan <leon.tan004@gmail.com>
2021-12-24 15:09:43 -0600

    Update StaticStack.java

--- Stack/StaticStack.java
+++ Stack/StaticStack.java
@@ -1,1 +1,65 @@
+public class StaticStack {
 
+    // Initialize the array to be of size 5
+    int[] stack = new int[5];
+    int top=0;
+
+    public void push(int data){
+
+        // To check if the stack is full
+        if(top >= stack.length){
+            System.out.println("The stack is full");
+            return;
+        }
+
+        // Assign the value to stack
+        stack[top] = data;
+        top++;
+    }
+
+    // To check if the stack is empty or not
+    public boolean isEmpty(){
+        if(top <= 0){
+            return true;
+        }
+        return false;
+    }
+
+    // Return the number of elements in stack
+    public int size(){
+        return top;
+    }
+
+    // Getting the last element of stack
+    public int peek(){
+        if(top <= 0){
+            System.out.println("There is currently no element in the stack.");
+            return -1;
+        }
+        return stack[top-1];
+    }
+
+    // Removing the last element of stack
+    public int pop(){
+        if(top <= 0){
+            System.out.println("The stack is empty.");
+            return -1;
+        }
+
+        top--;
+        int data = stack[top];
+        return data;
+    }
+
+    // Showing elements currently in stack
+    public void show(){
+        if(top <= 0){
+            System.out.println("There is currently no element in the stack.");
+            return;
+        }
+
+        for(int elem : stack){
+            System.out.println(elem + " ");
+        }
+    }
+}
