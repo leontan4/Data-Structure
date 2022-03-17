dd9374052370f67caf2b0ff2b372a4534d7a7211
Leon Tan <leon.tan004@gmail.com>
2022-03-16 20:57:25 -0500

    Update Tree.java

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -1,3 +1,5 @@
+package Trees;
+
 public class Tree {
     private TreeNode root;
 
@@ -6,6 +8,35 @@ public class Tree {
             root = new TreeNode(value);
         } else{
             root.insert(value);
+        }
+    }
+
+    public TreeNode get(int value){
+        if(root != null){
+            return root.get(value);
+        }
+        return null;
+    }
+
+    public int min(){
+        if(root == null){
+            return -1;
+        } else{
+            return root.min();
+        }
+    }
+
+    public int max(){
+        if(root == null){
+            return -1;
+        } else{
+            return root.max();
+        }
+    }
+
+    public void traverseInOrder(){
+        if(root != null) {
+            root.traverseInOrder();
         }
     }
 }
